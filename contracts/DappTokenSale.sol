cba6b3b12337aff0a8da90909f24cdd1cd0ad662
Maniddarky <42319136+Maniddarky@users.noreply.github.com>
2020-10-17 10:56:00 +0530

    Update DappTokenSale.sol
    fixed dapptokensale

--- contracts/DappTokenSale.sol
+++ contracts/DappTokenSale.sol
@@ -1,41 +1,62 @@
-pragma solidity ^0.4.2;
-
+pragma solidity >=0.5.0;
 import "./DappToken.sol";
+contract DappTokenSale{
 
-contract DappTokenSale {
-    address admin;
+    address payable admin;
     DappToken public tokenContract;
     uint256 public tokenPrice;
     uint256 public tokensSold;
 
     event Sell(address _buyer, uint256 _amount);
+    event EndSale(uint256 _totalAmountSold);
 
-    function DappTokenSale(DappToken _tokenContract, uint256 _tokenPrice) public {
+    constructor (DappToken _tokenContract, uint256 _tokenPrice) public
+    {
+        //assign an admin
         admin = msg.sender;
+        //tokrn contract
         tokenContract = _tokenContract;
+        //set token price
         tokenPrice = _tokenPrice;
+
     }
 
+    //multiply
     function multiply(uint x, uint y) internal pure returns (uint z) {
         require(y == 0 || (z = x * y) / y == x);
     }
 
+
+    //buying token
     function buyTokens(uint256 _numberOfTokens) public payable {
+        //Require that value is equal to tokens
         require(msg.value == multiply(_numberOfTokens, tokenPrice));
-        require(tokenContract.balanceOf(this) >= _numberOfTokens);
+
+
+        //reqiure that the contract has enough tokens
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+
+        //reqiure a transaction is successful
         require(tokenContract.transfer(msg.sender, _numberOfTokens));
 
-        tokensSold += _numberOfTokens;
 
-        Sell(msg.sender, _numberOfTokens);
+        //keep track of number of tokensSold
+        tokensSold += _numberOfTokens;
+        //emit a sell event
+        emit Sell(msg.sender, _numberOfTokens);
+
     }
 
+    //Ending the DappToken Sale 
     function endSale() public {
-        require(msg.sender == admin);
-        require(tokenContract.transfer(admin, tokenContract.balanceOf(this)));
 
-        // UPDATE: Let's not destroy the contract here
-        // Just transfer the balance to the admin
+        //Enquire admin
+        require(msg.sender == admin);
+        //Transfer remaining DappTokens to the admin
+        require(tokenContract.transfer(admin, tokenContract.balanceOf(address(this))));
+        //Destroy the contract
         admin.transfer(address(this).balance);
+
+        emit EndSale(tokensSold);
     }
 }
