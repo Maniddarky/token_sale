4807f67c803737879c90e43797ed200ab1b8a4f0
Maniddarky <42319136+Maniddarky@users.noreply.github.com>
2020-10-17 10:54:45 +0530

    Update DappToken.sol
    Updated dapptoken

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,16 +1,20 @@
-pragma solidity ^0.4.2;
+pragma solidity>=0.5.0;
 
-contract DappToken {
-    string  public name = "DApp Token";
-    string  public symbol = "DAPP";
-    string  public standard = "DApp Token v1.0";
+contract DappToken{
+    //name
+    string public name = "DApp Token";
+    //symbol
+    string public symbol = "DAPP";
+    //standard 
+    string public standard = "DApp Token v1.0";
     uint256 public totalSupply;
 
     event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 _value
+       address indexed _from,
+       address indexed _to,
+       uint256 _value
     );
+    //approve event
 
     event Approval(
         address indexed _owner,
@@ -19,43 +23,58 @@ contract DappToken {
     );
 
     mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
+    mapping (address => mapping(address => uint256))  public allowance;
 
-    function DappToken (uint256 _initialSupply) public {
-        balanceOf[msg.sender] = _initialSupply;
+    //allowance
+
+    constructor (uint256 _initialSupply) public {
+        balanceOf[msg.sender]=_initialSupply;
         totalSupply = _initialSupply;
+        //allocate the initialSupply
     }
+    //transfer
+    
+    function transfer(address _to,uint256 _value) public returns (bool success){
+        //Exception if account doesn't have enough
+        require(balanceOf[msg.sender]>= _value);
 
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender]-=_value;
+        balanceOf[_to]+=_value;
 
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-
-        Transfer(msg.sender, _to, _value);
-
+        //Transfer event
+        emit Transfer(msg.sender,_to,_value);
+        //return boolean
+        return true;   
+    }
+    
+    //approve
+    function approve(address _spender,uint256 _value) public returns (bool success) {
+        //allowance
+        allowance[msg.sender][_spender]=_value;
+        //approve event
+        emit Approval(msg.sender,_spender,_value);
         return true;
+
     }
 
-    function approve(address _spender, uint256 _value) public returns (bool success) {
-        allowance[msg.sender][_spender] = _value;
+    //tranfer from 
+    function transferFrom(address _from ,address _to, uint256 _value) public returns (bool success){
 
-        Approval(msg.sender, _spender, _value);
-
-        return true;
-    }
-
-    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
         require(_value <= balanceOf[_from]);
         require(_value <= allowance[_from][msg.sender]);
-
-        balanceOf[_from] -= _value;
-        balanceOf[_to] += _value;
-
-        allowance[_from][msg.sender] -= _value;
-
-        Transfer(_from, _to, _value);
-
+        //Require _from has enough tokens
+        //Require the allowance is big enough
+        //Change the balance
+        balanceOf[_from]-= _value;
+        balanceOf[_to]+= _value;
+        //update the allowance
+        allowance[_from][msg.sender]-=_value;
+        //Transfer event
+        emit Transfer(_from, _to, _value);
+        //return true
         return true;
+
+
     }
+
 }
